945a55d4da4bd15cf19b06f85c64d3ebbed388ff
Aditya Mewada <eng.adityamewada@gmail.com>
2025-02-19 23:59:17 +0530

    material ui and react

---
+++ ptkadmin/src/App.tsx
@@ -0,0 +1,12 @@
+// import Admin from './admin'
+import AdminDashboard from './adminDashboard'
+
+const App = () => {
+
+  return <div>
+     {/* <Admin /> */}
+     <AdminDashboard/>
+  </div>
+}
+
+export default App
